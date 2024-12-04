80add2a69232929c12660dd1330d24dda3721eb8
Jose Hernandez <josehdzibarra@hotmail.com>
2024-12-04 00:48:12 -0600

    Fotos de corrección de conexiones hardware

--- tests/Xbee.py
+++ tests/Xbee.py
@@ -3,10 +3,13 @@ from utime import sleep
 from time import time
 
 
+tiempo = 100 # Segundos hasta terminar el programa
 # Leer UART 
+# uart = UART(1,baudrate=9600, tx=Pin(8),rx=Pin(9))
 uart = UART(0,baudrate=9600,tx=Pin(12),rx=Pin(13))
-sleep(1)
-timeout = time()+5
+# uart = UART(0,baudrate=9600)
+sleep(0.1)
+timeout = time()+tiempo
 while True:
     print("Hello World from Raspberry Pi Pico\n\n".encode())
     uart.write("Hello World from Raspberry Pi Pico\n\n".encode())
